8904e604f09bc35b310c831aac90ed8a46d6b3fb
damico williams <damicowilliams@gmail.com>
2020-06-19 19:14:19 -0700

    first push

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {removeFeature} from '../actions';
 
 const AddedFeature = props => {
+
+  const handleChange = e => {
+    e.preventDefault()
+    props.removeFeature(props.feature)
+  }
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button onClick={handleChange} className="button">X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+// const mapStateToProps = state => {
+//   return {
+//     name: state.car
+//   }
+// }
+// getting state from AddedFeatures
+
+export default connect(null, {removeFeature})(AddedFeature);
